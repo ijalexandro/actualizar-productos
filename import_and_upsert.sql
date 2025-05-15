0b16e9a8d71a6cef24b97d9562484473e1fedf49
ijalexandro <ivan.j.alexandro@gmail.com>
2025-05-15 17:45:45 -0300

    Create import_and_upsert.sql

---
+++ import_and_upsert.sql
@@ -0,0 +1,25 @@
+BEGIN;
+TRUNCATE staging_productos;
+
+COPY staging_productos
+  FROM 'supstorage://raw-csv/productos.csv'
+  WITH (FORMAT csv, HEADER true);
+
+INSERT INTO productos_catalogo AS tgt (
+  codigo,
+  precio_final_con_iva,
+  precio_final_con_desc_e,
+  total_disponible_venta
+)
+SELECT
+  numero_producto || store_number AS codigo,
+  trunc(precio_final_con_iva::numeric, 2),
+  trunc(precio_final_con_desc_e::numeric, 2),
+  round(total_disponible_venta::numeric)
+FROM staging_productos
+ON CONFLICT (codigo) DO UPDATE
+  SET precio_final_con_iva      = EXCLUDED.precio_final_con_iva,
+      precio_final_con_desc_e   = EXCLUDED.precio_final_con_desc_e,
+      total_disponible_venta    = EXCLUDED.total_disponible_venta;
+
+COMMIT;
